14b0db4e5d4db445ea0f75f89f938e54db447c1f
YanLiuuuu <1932493957@qq.com>
2025-02-17 21:26:44 +0800

    Update run_cnn_k_mysparsemil_new.py

--- run_cnn_k_mysparsemil_new.py
+++ run_cnn_k_mysparsemil_new.py
@@ -17,6 +17,7 @@ from keras.regularizers import l1l2
 import inbreast
 #import googlenet
 from convnetskeras.convnets import preprocess_image_batch, convnet
+# 基于稀疏密度MIL
 np.random.seed(1)
 #srng = RandomStreams(1)
 fold = 4# 4
@@ -26,16 +27,16 @@ nb_epoch = 500
 batch_size = 80
 l2factor = 5e-6
 l1factor = 0#2e-7
-usedream = False
+usedream = False # 是否使用某种特定方法
 weighted = False #True
 noises = 50
 data_augmentation = True
 modelname = 'alexnet' # miccai16, alexnet, levynet, googlenet
 pretrain = True
 #sparsemil = True
-sparsemil = True
-sparsemill1 = 1e-4 #1e-5
-sparsemill2 = 0.0 #1e-2
+sparsemil = True 是否使用稀疏多实例学习
+sparsemill1 = 1e-4 #1e-5 # 稀疏MIL的L1正则化系数
+sparsemill2 = 0.0 #1e-2 # 稀疏MIL的L2正则化系数
 savename = modelname+'60new_fd'+str(fold)+'_vf'+str(valfold)+'_lr'+str(lr)+'_l2'+str(l2factor)+'_l1'\
 +str(l1factor)+'_ep'+str(nb_epoch)+'_bs'+str(batch_size)+'_w'+str(weighted)+'_dr'+str(usedream)+str(noises)+str(pretrain)+'_sp'+str(sparsemil)+str(sparsemill1)+str(sparsemill2)+'ft'
 print(savename)
@@ -46,11 +47,14 @@ img_rows, img_cols = 227, 227
 img_channels = 1
 
 # the data, shuffled and split between train and test sets
+# 从 inbreast 模块加载数据，包括训练集、验证集和测试集
 trX, y_train, teX, y_test, teteX, y_test_test = inbreast.loaddataenhance(fold, 5, valfold=valfold, valnum=60)
+# 将标签数据重塑为二维数组
 trY = y_train.reshape((y_train.shape[0],1))
 teY = y_test.reshape((y_test.shape[0],1))
 teteY = y_test_test.reshape((y_test_test.shape[0],1))
 ''' for finetune '''
+# 将验证集数据合并到训练集中，用于微调 finetune
 trX = np.concatenate((trX, teX), axis=0)
 y_train = np.concatenate((y_train,y_test), axis=0)
 teX = teteX
@@ -63,6 +67,7 @@ weights = np.array((ratio, 1-ratio))
 #trYori = np.concatenate((1-trY, trY), axis=1)
 #teY = np.concatenate((1-teY, teY), axis=1)
 #teteY = np.concatenate((1-teteY, teteY), axis=1)
+# 将输入数据重塑为适合卷积神经网络的形状（通道在前格式）
 X_train = trX.reshape(-1, img_channels, img_rows, img_cols)
 X_test = teX.reshape(-1, img_channels, img_rows, img_cols)
 X_test_test = teteX.reshape(-1, img_channels, img_rows, img_cols)
@@ -90,6 +95,7 @@ print(X_train.mean(), X_test.mean(), X_test_test.mean())
 #ratio = Y_train[:,:].sum()*1./(trY.shape[0]*6*6)*1.
 #weights = np.array((ratio, 1-ratio))
 #print('tr my mil ratio'+str(ratio))
+# 将标签数据转换为one-hot编码格式
 Y_train = np_utils.to_categorical(y_train, nb_classes)
 Y_test = np_utils.to_categorical(y_test, nb_classes)
 Y_test_test = np_utils.to_categorical(y_test_test, nb_classes)
@@ -98,6 +104,9 @@ print('Y_train shape:', Y_train.shape)
 print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'val samples')
 print(X_test_test.shape[0], 'test samples')
+# 模型构建
+# 如果模型是 alexnet，则将单通道图像扩展为三通道（AlexNet 需要三通道输入
+# 如果使用预训练模型，则加载预训练权重
 model = Sequential()
 if modelname == 'alexnet':
   X_train_extend = np.zeros((X_train.shape[0],3, 227, 227))
@@ -136,6 +145,7 @@ if modelname == 'alexnet':
     model = convnet('alexnet', outdim=2, l1=l1factor,l2=l2factor, sparsemil=sparsemil, sparsemill1=sparsemill1, sparsemill2=sparsemill2)
 
 # let's train the model using SGD + momentum (how original).
+# 模型编译 使用Adam优化器编译模型，损失函数为分类交叉熵，评估指标为准确率
 sgd = Adam(lr=lr) #SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 #model.load_weights("alexnet_fd4_vf1_lr5e-05_l25e-06_l10_ep500_bs80_wFalse_drFalse50True_spTrue1e-050.0acc0.916666666667ep241.hdf5")
 model.compile(loss='categorical_crossentropy', #categorical_crossentropy',
@@ -146,6 +156,7 @@ print(model.summary())
 #{val_prec:.2f}-{val_reca:.2f}-{val_f1:.2f}.hdf5'
 #checkpoint0 = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='max')
 #checkpoint1 = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+# 回调函数 用于在训练过程中记录损失，准确率，AUC，精确率，召回率和F1分数
 checkpoint0 = LossEpoch(savename, validation_data=(X_test, Y_test), interval=1)
 checkpoint1 = ACCEpoch(savename, validation_data=(X_test, Y_test), interval=1)
 checkpoint2 = AUCEpoch(savename, validation_data=(X_test, Y_test), interval=1)
@@ -162,6 +173,9 @@ X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 #X_train /= 255
 #X_test /= 255
+# 数据增强
+# 如果启用数据增强，则使用ImageDataGenerator对训练数据进行实时增强
+# 如果启用类别权重，则在训练时使用类别权重
 if not data_augmentation:
   print('Not using data augmentation.')
   model.fit(X_train, Y_train,
